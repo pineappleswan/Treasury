b9b69e8da4f97509f036d3af7ecda0fa5f872a7b
ifeelagood <thicccatto3@gmail.com>
2024-03-12 20:20:44 +1100

    fixed start.sh

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,17 +1,10 @@
 #!/bin/bash
 
-if [[! -d proc ]]; then mkdir proc; fi
+npm_cmd=run dev
+npx_cmd=tsx server/index.ts --port 3001 --dev
 
-start_process() {
-  cmd="$1 $2"
-  cin="proc/$1.cin"
-  out="proc/$1.out"
-  echo -n > $cin
-  echo -n > $out
-  ($cmd < $cin 2>&1 $out) &
-  echo "started process $cmd with PID $!"
-}
-
-start_process "npm" "run dev"
-start_process "npx" "tsx server/index.ts --port 3001 --dev"
-
+(npm $npm_cmd) &
+echo "$!    npm    $npm_cmd"
+(npx $npx_cmd) &
+echo "$!    npx    $npx_cmd"
+wait
