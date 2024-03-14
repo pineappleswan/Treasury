be5afa8fcd05d70e2ea261ec9a1dfa0f45b8a712
ifeelagood <thicccatto3@gmail.com>
2024-03-14 22:12:38 +1100

    fixed start.sh

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
 
-npm_cmd=run dev
-npx_cmd=tsx server/index.ts --port 3001 --dev
+(npm run build) &
+npx tsx server/index.ts --port 3001 --secure-cookies false
 
-(npm $npm_cmd) &
-echo "$!    npm    $npm_cmd"
-(npx $npx_cmd) &
-echo "$!    npx    $npx_cmd"
 wait
