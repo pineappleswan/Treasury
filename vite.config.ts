c8b4ce3c6e4f23487ef03c84d541277ac9919cb4
pineappleswan <pineappleswan@protonmail.com>
2024-05-08 21:27:28 +1000

    Reupload

--- vite.config.ts
+++ vite.config.ts
@@ -2,8 +2,8 @@ import { defineConfig } from "vite";
 import solidPlugin from "vite-plugin-solid";
 import solidSvg from "vite-plugin-solid-svg";
 
-const DEV_SERVER_PORT = 3000; // The port you connect to to use the vite server. e.g localhost:3000
-const DEV_SERVER_PROXY_URL = `http://0.0.0.0:3001`; // You can change the url and its port
+const DEV_SERVER_PORT = 3000; // The port you connect to in order to use the vite server. e.g localhost:3000
+const DEV_SERVER_PROXY_URL = `http://0.0.0.0:3001`; // The proxy the vite server will use. You can change the url and its port.
 
 export default defineConfig({
   plugins: [
@@ -17,7 +17,7 @@ export default defineConfig({
     port: DEV_SERVER_PORT,
     host: "0.0.0.0",
     proxy: {
-      // Redirect API and CDN requests to node.js server (ONLY FOR DEVELOPMENT PURPOSES)
+      // Redirect API and CDN requests to the node.js server
       "/api/": { target: DEV_SERVER_PROXY_URL, changeOrigin: true },
       "/cdn/": { target: DEV_SERVER_PROXY_URL, changeOrigin: true }
     }
